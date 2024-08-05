3a0d60c84dabbe60252ff81cc3bfb2a2b2a37fc6
Martin336699 <martinschmidt369@t-online.de>
2024-08-05 23:08:26 +0200

    Initial commit of my Websit

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Lade Umgebungsvariablen
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER || "martinschmit369@gmail.com",
+        pass: process.env.EMAIL_PASS || "piwm xvhu hxvq hqck", // Verwende hier dein App-Passwort
+    },
+    tls: {
+        rejectUnauthorized: false // Diese Zeile hinzufügen
+    }
+});
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+});
+
+router.post("/contact", (req, res) => {
+    const name = req.body.firstName + " " + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+
+    const mail = {
+        from: name,
+        to: process.env.EMAIL_USER || "bronco994@web.de",
+        subject: "Contact Form Submission - Portfolio",
+        html: `<p>Name: ${name}</p> <p>Email: ${email}</p> <p>Phone: ${phone}</p> <p>Message: ${message}</p>`,
+    };
+
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json({ code: 500, status: "Error sending message", error });
+        } else {
+            res.json({ code: 200, status: "Message Sent" });
+        }
+    });
+});
+
+app.listen(5000, () => console.log("Server Running on port 5000"));
---
+++ src/components/Projects.jsx
@@ -0,0 +1,114 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import calc from "../assets/img/calc.jpg";
+import todo from "../assets/img/todo.jpg";
+import game from "../assets/img/game.jpg";
+import household from "../assets/img/household.jpg";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+
+
+export const Projects = () => {
+
+  const apps = [
+    {
+      title: "Calculator",
+      description: "Design & Development",
+      imgUrl: calc,
+      link: "https://github.com/Martin336699/js-tutorial"
+    },
+    {
+      title: "To-Do-List",
+      description: "Design & Development",
+      imgUrl: todo,
+      link: "https://github.com/Martin336699/udemi-js-course"
+    },
+    {
+      title: "Haushaltsbuch",
+      description: "Design & Development",
+      imgUrl: household,
+      link: "https://github.com/Martin336699/udemi-js-course"
+    }
+  ];
+  const games = [
+    {
+      title: "Rock-Paper-Scissors-Game",
+      description: "Design & Development",
+      imgUrl: game
+    }
+  ];
+  const websites = [
+    {
+      title: "Free Project",
+      description: "Design & Development",
+      imgUrl: projImg1
+    },
+    {
+      title: "Free Project",
+      description: "Design & Development",
+      imgUrl: projImg2
+    }
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col>
+          <h2>Projects</h2>
+          <p>In this section, I present a selection of my projects that demonstrate my programming    skills across various areas. From web development to software applications and other  programming projects—each project reflects my passion for coding and my ability to solve complex problems.</p>
+          <Tab.Container id="projects-tabs" defaultActiveKey="first">
+            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Apps</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Games</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">Websites</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <Row>
+                  {
+                    apps.map((app, index) => {
+                      return (
+                        <ProjectCard key={index} {...app} />
+                      )
+                    })
+                  }
+                </Row>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <Row>
+                  {
+                    games.map((game, index) => {
+                      return (
+                        <ProjectCard key={index} {...game} />
+                      )
+                    })
+                  }
+                </Row>
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+              <Row>
+                  {
+                    websites.map((site, index) => {
+                      return (
+                        <ProjectCard key={index} {...site} />
+                      )
+                    })
+                  }
+                </Row>
+              </Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      
+    </section>
+  )
+}

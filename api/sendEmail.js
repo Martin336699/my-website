30ce766c0be194b195cf7f9efaa406f0963846ab
Martin336699 <martinschmidt369@t-online.de>
2024-08-07 23:39:31 +0200

    api verzeichnis addet

---
+++ api/sendEmail.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+export default async function handler(req, res) {
+if (req.method === 'POST') {
+const { name, email, message } = req.body;
+
+const transporter = nodemailer.createTransport({
+host: 'smtp.gmail.com', // Ersetzen Sie dies durch Ihren SMTP-Host
+port: 587,
+auth: {
+user: 'martinschmit369@gmail.com', // Ersetzen Sie dies durch Ihren SMTP-Benutzernamen
+pass: 'piwm xvhu hxvq hqck', // Ersetzen Sie dies durch Ihr SMTP-Passwort
+},
+});
+
+const mailOptions = {
+from: 'martinschmit369@gmail.com', // Ihre E-Mail-Adresse
+to: 'bronco994@web.de', // Ziel-E-Mail-Adresse
+subject: 'New Contact Form Submission',
+text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+};
+
+try {
+await transporter.sendMail(mailOptions);
+res.status(200).json({ message: 'Email sent successfully' });
+} catch (error) {
+res.status(500).json({ error: 'Failed to send email' });
+}
+} else {
+res.status(405).json({ error: 'Method not allowed' });
+}
+}
